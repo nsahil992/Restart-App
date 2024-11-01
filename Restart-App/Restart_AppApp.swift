371a1c4c342066f8803174ab3be3d334f9975d5a
Sahil Naik <nsahil992@gmail.com>
2024-11-01 15:24:44 +0530

    feat: cp

---
+++ Restart-App/Restart_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_AppApp.swift
+//  Restart-App
+//
+//  Created by Sahil on 01/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Restart_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Restart-AppTests/Restart_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_AppTests.swift
+//  Restart-AppTests
+//
+//  Created by Sahil on 01/11/24.
+//
+
+import Testing
+@testable import Restart_App
+
+struct Restart_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
